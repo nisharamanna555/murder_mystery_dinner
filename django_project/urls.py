f444411011ad80d8ee8bb8ad7be4646e4cbca841
nisharamanna555 <nisha.ramanna.20@gmail.com>
2025-05-08 10:33:30 -0400

    routing fix

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic.base import TemplateView #change this
+from django.views.generic.base import TemplateView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path("", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
+    path("home/", login_required(TemplateView.as_view(template_name="home.html")), name="home"),
     path("admin/", admin.site.urls),
-    path("accounts/", include("django.contrib.auth.urls")),
-    path("", TemplateView.as_view(template_name="home.html"), name="home"),
 ]
